e28131821a6d4ef5ba3aca0730b0de3ac4d64e20
mahesh <maheshkuraganti9@gmail.com>
2024-06-03 12:33:10 +0530

    ami modified in ec2 tf and commited

--- createec2instance/ec2.tf
+++ createec2instance/ec2.tf
@@ -6,7 +6,7 @@ provider "aws" {
 #Resource block
 resource "aws_instance" "myec2instance" {
     instance_type = "t2.micro"
-    ami = "ami-00fa32593b478ad6e" 
+    ami = "ami-00beae93a2d981137"
 
 tags = {
     "Name" = "maheshec2"
