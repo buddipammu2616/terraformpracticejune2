c1711fa8f2ea58ee665913cc9ad1493641487702
mahesh <maheshkuraganti9@gmail.com>
2024-06-03 12:13:03 +0530

    second vpc created and commited

--- vpcmanifest/vpc.tf
+++ vpcmanifest/vpc.tf
@@ -4,5 +4,10 @@ resource "aws_vpc" "myvpc" {
     tags = {
       "Name" = "myvpc"
     }
-  
+}
+resource "aws_vpc" "myvpc1" {
+    cidr_block = "10.1.0.0/16"
+    tags = {
+      "Name" = "myvpc1"
+    }
 }
