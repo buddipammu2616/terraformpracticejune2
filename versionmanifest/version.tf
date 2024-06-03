233ef82e17cd9950c8398443c4973c74baecf3a1
mahesh <maheshkuraganti9@gmail.com>
2024-06-03 11:25:26 +0530

    versionmanifest commited

---
+++ versionmanifest/version.tf
@@ -0,0 +1,17 @@
+#Terraform block
+terraform {
+  required_version = "~>1.8.4"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>5.0"
+    }
+  }
+}
+
+#Provider Block
+provider "aws" {
+    region = "ap-south-1"
+    profile = "default"
+  
+}
